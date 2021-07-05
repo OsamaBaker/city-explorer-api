122abb683ecf4488c20f8ba0e57a7e58a78c40d1
Osama Baker <edewanjy@hotmail.com>
2021-07-05 18:51:58 +0300

    Completed

--- server.js
+++ server.js
@@ -18,18 +18,32 @@ server.get('/', (req, res) => {
 
 server.get('/weather', (req, res) => {
     let city = req.query.searchQuery
-    console.log(city)
     let lat = req.query.lat;
     let lon = req.query.lon;
+    
+    let lookForData = weatherData.find(element => {
+        if( element.city_name === city ) {
+            return element.city_name
+        }
+    })
+    res.status(200).send(lookForData);
 
-    weatherData.find(element => {
-       if( element.cityName == city ) {
-           return city
-       } else{
-           return res.status(404).send('NOT FOUND')
-       }
+    const cityObj = lookForData.data.map(day => {
+        return new Forecast(day.valid_date, day.description)
     })
 })
+
+server.get('/*', (req, res) => {
+    res.status(404).send('NOT FOUND')
+})
+
+class Forecast {
+    constructor(date, description){
+        this.date = date,
+        this.description = description
+    }
+}
+
 
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
